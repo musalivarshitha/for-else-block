6bb3a47b83be422a4c10d55bfb3ccf9577e260b2
musali_varshitha_reddy <musalivarshithareddy@gmail.com>
2025-08-12 10:52:32 +0530

    Update prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -1,6 +1,10 @@
 n=int(input())
-for divisor in range(2,n//2+1):
-    if n%divisor==0:
-        print('not a prime num')
+if n>1:
+    for divisor in range(2,n//2+1):
+        if n%divisor==0:
+            print('not a prime num')
+            break
     else:
         print('its a prime num')
+else:
+    print('n is not a prime num')
